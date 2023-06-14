fba6e2434249bd498485ee573633c0ff9a80af41
BrunaLaysa <bruna.barboza@sptech.school>
2023-06-14 06:42:22 -0300

    correcao de problemas

--- site/public/js/paginaInicial.js
+++ site/public/js/paginaInicial.js
@@ -1,6 +1,6 @@
 let currentSlide = 0;
 const categoriaSelect = document.getElementById("select_categoria");
-//
+
 const sessoes = document.querySelectorAll('.section_tattoo')
 const slides = document.querySelectorAll('.slide');
 const likeButtons = document.querySelectorAll('.like-btn');
@@ -8,7 +8,7 @@ console.log(likeButtons);
 let categoriaSelecionada = 'todas'
 let categorias = [];
 let tatuagens = [];
-let tatuagensCurtidasUsuario;
+let tatuagensCurtidasUsuario = [];
 
 document.addEventListener('DOMContentLoaded', async () => {
   await carregarDadosIniciais();
@@ -17,143 +17,116 @@ document.addEventListener('DOMContentLoaded', async () => {
 async function carregarDadosIniciais() {
   this.categorias = await carregarCategorias();
   this.tatuagensCurtidasUsuario = await carregarCurtidas();
+  if (!this.tatuagensCurtidasUsuario) {
+    this.tatuagensCurtidasUsuario = []; // Define como um array vazio se não houver curtidas
+  }
   await carregarTatuagens();
   atualizarTatuagens();
 }
+
+
 const carregarCategorias = async () => {
-  fetch("/categorias/listar", {
-    method: "GET"
-  }).then(function (resposta) {
+  try {
+    const resposta = await fetch("/categorias/listar", {
+      method: "GET"
+    });
+
     if (resposta.ok) {
-        resposta.json().then(json => {
-          this.categorias = json;
-          for(let i = 0; i < json.length; i++) {
-            console.log("aaa123");
-            console.log(json[i].idCategoria);
-            categoriaSelect.innerHTML = categoriaSelect.innerHTML +
-                '<option value="' + json[i].idCategoria + '">' + json[i].nomeCategoria + '</option>';
-          }
-          return json;
-        });
+      const json = await resposta.json();
+      for (let i = 0; i < json.length; i++) {
+        console.log("aaa123");
+        console.log(json[i].idCategoria);
+        categoriaSelect.innerHTML = categoriaSelect.innerHTML +
+          '<option value="' + json[i].idCategoria + '">' + json[i].nomeCategoria + '</option>';
+      }
+      return json;
     } else {
-        console.log("Houve um erro ao listar as categorias");
+      console.log("Houve um erro ao listar as categorias");
+      return [];
     }
-
-}).catch(function (erro) {
+  } catch (erro) {
     console.log(erro);
-});
-
-
+    return [];
+  }
 }
 
 const carregarCurtidas = async () => {
-  const resposta = await fetch(`/curtidas/usuario/${sessionStorage.getItem('ID_USUARIO')}`, {
-    method: "GET"
-  });
+  try {
+    const resposta = await fetch(`/curtidas/usuario/${sessionStorage.getItem('ID_USUARIO')}`, {
+      method: "GET"
+    });
 
-  if (resposta.ok) {
-    const json = await resposta.json();
-    return json;
-  } else {
-    console.log("Houve um erro ao trazer as curtidas do usuário");
+    if (resposta.ok) {
+      const json = await resposta.json();
+      return json;
+    } else {
+      console.log("Houve um erro ao trazer as curtidas do usuário");
+      return []; // Retorna um array vazio em caso de erro
+    }
+  } catch (erro) {
+    console.log(erro);
+    return []; // Retorna um array vazio em caso de erro
   }
 };
 
 const carregarTatuagens = async () => {
-  fetch("/tatuagens/listar", {
-    method: "GET"
-}).then(function (resposta) {
-    if(resposta.ok) {
-      resposta.json().then(json => {
-          json.forEach((tattoo) => {
-            let sessoes = document.querySelectorAll('.section_tattoo');
-            let sessaoTattoo;
-            for(let i = 0; i < sessoes.length; i++) {
-                if(tattoo.fkCategoria === Number(sessoes[i].getAttribute('data-categoria'))) {
-                    sessaoTattoo =  sessoes[i];
-                } 
-            }
-            
-            let curtidas = this.tatuagensCurtidasUsuario;
-            let classLiked = curtidas.some((curtida) => curtida.fkTatuagem === tattoo.idTatuagem) ? 'liked' : '';
-            sessaoTattoo.innerHTML = sessaoTattoo.innerHTML +  `
-              <div class="card_tattoo">
-                <img src="${tattoo.caminhoImagem}">
-                <div class="div_reacao">
-                  <button class="like-btn ${classLiked}" onClick="curtir(${tattoo.idTatuagem})"><i class="fas fa-heart like_icon"></i></button>
-                <span>0</span>
-                <button><i class="far fa-comment coment_icon"></i></button>
-                </div>
-              </div>
-            `
-          });
-      });
+  try {
+    const resposta = await fetch("/tatuagens/listar", {
+      method: "GET"
+    });
+
+    if (resposta.ok) {
+      const json = await resposta.json();
+      this.tatuagens = json;
     } else {
       console.log("Houve um erro ao listar as tatuagens");
+    }
+  } catch (erro) {
+    console.log(erro);
   }
-});
+};
 
-}
-
-function curtir(idTatuagem){
-  var fkTatuagem = idTatuagem;
-  var fkUsuario = sessionStorage.getItem('ID_USUARIO');
-  let urlCurtidas = `/curtidas/usuario/${fkUsuario}`
+function curtir(idTatuagem) {
+  const fkTatuagem = idTatuagem;
+  const fkUsuario = sessionStorage.getItem('ID_USUARIO');
+  const urlCurtidas = `/curtidas/usuario/${fkUsuario}`;
   console.log(idTatuagem);
 
   fetch(urlCurtidas, {
     method: "POST",
     headers: {
-        "Content-Type": "application/json"
+      "Content-Type": "application/json"
     },
     body: JSON.stringify({
-        fkTatuagem: fkTatuagem
+      fkTatuagem: fkTatuagem
     })
   }).then((res) => {
+    if (res.ok) {
+      // Atualizar a lista de curtidas localmente
+      const novaCurtida = {
+        fkTatuagem: fkTatuagem
+      };
+      this.tatuagensCurtidasUsuario.push(novaCurtida);
+
+      // Chamar a função para atualizar as tatuagens na página
+      atualizarTatuagens(this.tatuagensCurtidasUsuario);
+    } else {
+      console.log("Houve um erro ao curtir a tatuagem");
+    }
   });
-
-  carregarCurtidas();
 }
 
-
-  
-  
-function nextSlide() {
-    slides[currentSlide].classList.remove('active');
-    currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].classList.add('active');
-}
-setInterval(nextSlide, 7000);
-
-
-  
-categoriaSelect.addEventListener('change', () => {
-    categoriaSelecionada = categoriaSelect.value;
-
-    sessoes.forEach((sessao) => {
-        let categoriaImagem = sessao.getAttribute("data-categoria");
-
-        if(categoriaSelecionada === 'todas' || categoriaSelecionada === categoriaImagem) {
-            sessao.style.display = 'flex';
-        }
-        else{
-            sessao.style.display = 'none';
-        }
-        
-    })
-})
-
-function atualizarTatuagens() {
+function atualizarTatuagens(curtidas) {
   sessoes.forEach((sessao) => {
     const categoriaImagem = sessao.getAttribute("data-categoria");
 
     sessao.innerHTML = '';
 
     if (categoriaSelecionada === 'todas' || categoriaSelecionada === categoriaImagem) {
-      tatuagens.forEach((tattoo) => {
+      this.tatuagens.forEach((tattoo) => {
         if (tattoo.fkCategoria === Number(categoriaImagem)) {
-          const curtidas = this.tatuagensCurtidasUsuario;
-          const classLiked = curtidas.some((curtida) => curtida.fkTatuagem === tattoo.idTatuagem) ? 'liked' : '';
+          const curtidasTattoo = curtidas || this.tatuagensCurtidasUsuario;
+          const classLiked = curtidasTattoo && curtidasTattoo.length > 0 && curtidasTattoo.some((curtida) => curtida.fkTatuagem === tattoo.idTatuagem) ? 'liked' : '';
           sessao.innerHTML += `
             <div class="card_tattoo">
               <img src="${tattoo.caminhoImagem}">
@@ -172,6 +145,7 @@ function atualizarTatuagens() {
     }
   });
 }
+
 
 
 likeButtons.forEach(function(button) {
